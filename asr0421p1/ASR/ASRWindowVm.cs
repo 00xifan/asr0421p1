f8c11f726cf948d4b6cfd8a3a7ecaaa8195682fc
石琳 <@LAPTOP-4LOALES4>
2025-04-22 13:11:46 +0800

    添加项目文件。

---
+++ asr0421p1/ASR/ASRWindowVm.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asr0421p1.ASR
+{
+   public class ASRWindowVm : ObservableObject
+    {
+    }
+}
